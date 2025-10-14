dce904c97ec9321b36355f3b7c861662b21e6ecc
何雨洋 <13598752812@163.com>
2025-10-15 03:06:41 +0800

    完成部分InitializeResult 通信字段，涉及到的服务端部分过大，还没有完整的从原协议完成低版本迁移

---
+++ CSharp/McpServer/JsonRpc/Server/McpRequestHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace JsonRpc.Server
+{
+    public delegate ValueTask<TResult> McpRequestHandler<TParams, TResult>(
+        RequestContext<TParams> request,
+        CancellationToken cancellationToken);
+}
